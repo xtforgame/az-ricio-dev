xtforgame/az-webrtc-dev

Use the ricio library

src/front-end/containers/App/modelMap.js
@@ -1,5 +1,7 @@
 import { getHeaders } from '~/utils/HeaderManager';
 import ModelMap from '~/utils/rest-model/ModelMap';
+import wsProtocol from '~/utils/wsProtocol1';
+import { CancelToken } from 'common/ricio/front-end';
 
 const responseMiddleware = (response, info) => {
   if(response.status === 200 && response.data.error){
@@ -34,6 +36,22 @@ const modelsDefine = {
       },
       reducers: {
         getId: action => 'me', // action.data.user_id,
+      },
+    },
+  },
+  wsSessions: {
+    url: '/sessions',
+    names: { singular: 'wsSession', plural: 'wsSessions' },
+    extensionConfigs: {
+      wsEpics: {
+        wsProtocol,
+        CancelToken,
+      },
+      selectors: {
+        baseSelector: state => state.get('global').wsSessions,
+      },
+      reducers: {
+        getId: action => 'me', // action.data.userid,
       },
     },
   },

src/server/services/user-manager/GenericUserSessionManager.js
@@ -1,0 +1,170 @@
+import { LoggedInUser, UserSessionManager } from 'login-session-mgr';
+import ChannelManager from './ChannelManager';
+import fakeUserManager from '../../utils/fakeUserManager';
+
+export class UserInfo extends LoggedInUser {
+  constructor({
+    uid,
+    data = {},
+    sessions = {},
+  }) {
+    super({uid, data, sessions});
+    this.data.channelMap = null;
+  }
+
+  get userSessionManager(){
+    return this.data.userSessionManager;
+  }
+
+  mapSession(inFn){
+    let sessions = this.sessions;
+    // console.log('sessions :', sessions);
+    let fn = inFn || (() => {});
+    return Object.keys(sessions).map(key => {
+      return fn(key, sessions[key], sessions);
+    });
+  };
+
+  send(msg){
+    return this.mapSession((_, session, sessions) =>
+      session.data.rcPeer.send(msg)
+    );
+  }
+
+  joinChannel(channelArray){
+    return this.userSessionManager.peerJoinChannel(this, channelArray);
+  }
+
+  leaveChannel(channelArray){
+    return this.userSessionManager.peerLeaveChannel(this, channelArray);
+  }
+
+  inChannel(channel){
+    return this.userSessionManager.isPeerInChannel(this, channel);
+  }
+}
+
+export default class GenericUserSessionManager {
+  constructor() {
+    this.userSessionCounters = {};
+    this.chManager = new ChannelManager();
+    this.allPeers = new Map();
+    this.userSessionMgr = new UserSessionManager({
+      UserInfoClass: UserInfo,
+      onSessionLoggedIn: (newSession => {
+        // console.log('=================== newSession :', newSession);
+        newSession.data.rcPeer.setSession(newSession);
+      }),
+      onUserLoggedIn: (newUser => {
+        newUser.data.userSessionManager = this;
+        // console.log('=================== newUser :', newUser);
+      }),
+      onSessionLoggedOut: ((existedSession, reason) => {
+      }),
+      onUserLoggedOut: (existedUser => {
+      }),
+      onSessionUnexpectedLoggedOut: ((existedSession, reason) => {
+      }),
+      onSessionReloggedIn: ((reloggedInSession, newData) => {
+      }),
+      onSessionDuplicateLogin: ((existedSession, newSession, logoutExistedOne, denyLogin) => {
+        denyLogin();
+        logoutExistedOne();
+      }),
+    });
+  }
+
+  addPeer(rcPeer) {
+    let wsPeer = rcPeer.getWsPeer();
+    if(wsPeer){
+      this.allPeers.set(rcPeer.getWsPeer(), rcPeer);
+    }
+  }
+
+  loginWithToken(rcPeer, token) {
+    // const session = fakeUserManager.authenticateFromToken(data.token);
+    const session = fakeUserManager.verify(token);
+    if (session) {
+      console.log('session.userid :', session.userid);
+      const wsSessionNumber = this.userSessionCounters[session.userid] = this.userSessionCounters[session.userid] ?
+        this.userSessionCounters[session.userid] + 1 : 1;
+
+      const wsSession = fakeUserManager.authenticateFromToken(token, { wsSessionNumber });
+
+      return this.userSessionMgr.login(wsSession.userid, wsSession.token, {
+        session,
+        rcPeer,
+      })
+      .then(() => wsSession);
+    }
+    return Promise.reject();
+  }
+
+  loginWithPassword(rcPeer, body){
+    const { auth_type, password, username } = body;
+    return Promise.resolve(fakeUserManager.authenticate(auth_type, username, password));
+  }
+
+  get users(){
+    return this.userSessionMgr.users;
+  }
+
+  mapUser(inFn){
+    let users = this.userSessionMgr.users;
+    let fn = inFn || (() => {});
+    return Object.keys(users).map(key => {
+      return fn(key, users[key], users);
+    });
+  }
+
+  findUser(uid){
+    return this.users[uid];
+  }
+
+  peerJoinChannel(user, channelArray){
+    this.chManager.join(user, channelArray);
+    user.data.channelMap = this.chManager.getUserMetadata(user);
+    return true;
+  }
+
+  peerLeaveChannel(user, channelArray){
+    this.chManager.leave(user, channelArray);
+    user.data.channelMap = this.chManager.getUserMetadata(user);
+    return true;
+  }
+
+  isPeerInChannel(user, channel){
+    return this.chManager.isInChannel(user, channel)
+  }
+
+  getPeerChannelList(user){
+    return this.chManager.getUserMetadata(user).map(value => value);
+  }
+
+  removePeer(rcPeer) {
+    let wsPeer = rcPeer.getWsPeer();
+    if(wsPeer){
+      this.allPeers.delete(rcPeer.getWsPeer());
+    }
+  }
+
+  logout(rcPeer) {
+    this.removePeer(rcPeer);
+    let sessionId = rcPeer.getSessionId();
+    if(sessionId){
+      // console.log('removePeer');
+      return this.userSessionMgr.logout(sessionId);
+    }
+    return Promise.resolve();
+  }
+
+  unexpectedLogout(rcPeer) {
+    console.log('unexpectedLogout');
+    this.removePeer(rcPeer);
+    let sessionId = rcPeer.getSessionId();
+    if(sessionId){
+      return this.userSessionMgr.unexpectedLogout(sessionId, 'ConnectionLost');
+    }
+    return Promise.resolve();
+  }
+}

src/server/utils/fakeUserManager.js
@@ -20,7 +20,7 @@ class FakeUserManager {
         username: user.username,
         auth_type,
         privilege: user.privilege,
-        subject: `user:${user.id}:${0}`,
+        subject: `user:${user.id}:${rest.wsSessionNumber || 0}`,
         token_type: 'Bearer',
         ...rest,
       }),
@@ -34,6 +34,10 @@ class FakeUserManager {
     });
 
     this.register('admin', 'admin', 'Admin', 'admin');
+    
+    for (let index = 1; index <= 10; index++) {
+      this.register(`testUser${index}`, `testUser${index}`, `Test User ${index}`, 'user');
+    }
   }
 
   register(username, password, name, privilege){
@@ -54,15 +58,36 @@ class FakeUserManager {
     return user;
   }
 
-  authenticate(auth_type, username, password){
+  authenticate(auth_type, username, password, extraPayload){
     const user = this.users[username];
     if(auth_type !== 'basic' || !user){
       return null;
     }
 
     let session = this.jwtSessionHelper.createSession({
+      ...extraPayload,
       user,
       auth_type,
+    });
+
+    return session.info;
+  }
+
+  authenticateFromToken(token, extraPayload){
+    const tokenInfo = this.verify(token);
+    if(!tokenInfo){
+      return null;
+    }
+
+    const user = this.users[tokenInfo.username];
+    if(!user){
+      return null;
+    }
+
+    let session = this.jwtSessionHelper.createSession({
+      ...extraPayload,
+      user,
+      auth_type: tokenInfo.auth_type,
     });
 
     return session.info;

xtforgame/az-ricio-dev

src/front-end/containers/App/modelMap.js
@@ -1,5 +1,7 @@
 import { getHeaders } from '~/utils/HeaderManager';
 import ModelMap from '~/utils/rest-model/ModelMap';
+import wsProtocol from '~/utils/wsProtocol1';
+import { CancelToken } from 'ricio/front-end';
 
 const responseMiddleware = (response, info) => {
   if(response.status === 200 && response.data.error){
@@ -31,6 +33,22 @@ const modelsDefine = {
       },
       selectors: {
         baseSelector: state => state.get('global').sessions,
+      },
+      reducers: {
+        getId: action => 'me', // action.data.user_id,
+      },
+    },
+  },
+  wsSessions: {
+    url: '/sessions',
+    names: { singular: 'wsSession', plural: 'wsSessions' },
+    extensionConfigs: {
+      wsEpics: {
+        wsProtocol,
+        CancelToken,
+      },
+      selectors: {
+        baseSelector: state => state.get('global').wsSessions,
       },
       reducers: {
         getId: action => 'me', // action.data.user_id,

src/server/services/user-manager/GenericUserSessionManager.js
@@ -1,0 +1,176 @@
+import { LoggedInUser, UserSessionManager } from 'login-session-mgr';
+import ChannelManager from './ChannelManager';
+import fakeUserManager from '../../utils/fakeUserManager';
+
+export class UserInfo extends LoggedInUser {
+  constructor({
+    uid,
+    data = {},
+    sessions = {},
+  }) {
+    super({uid, data, sessions});
+    this.data.channelMap = null;
+  }
+
+  get userSessionManager(){
+    return this.data.userSessionManager;
+  }
+
+  mapSession(inFn){
+    let sessions = this.sessions;
+    // console.log('sessions :', sessions);
+    let fn = inFn || (() => {});
+    return Object.keys(sessions).map(key => {
+      return fn(key, sessions[key], sessions);
+    });
+  };
+
+  send(msg){
+    return this.mapSession((_, session, sessions) =>
+      session.data.rcPeer.send(msg)
+    );
+  }
+
+  joinChannel(channelArray){
+    return this.userSessionManager.peerJoinChannel(this, channelArray);
+  }
+
+  leaveChannel(channelArray){
+    return this.userSessionManager.peerLeaveChannel(this, channelArray);
+  }
+
+  inChannel(channel){
+    return this.userSessionManager.isPeerInChannel(this, channel);
+  }
+}
+
+export default class GenericUserSessionManager {
+  constructor() {
+    this.userSessionCounters = {};
+    this.chManager = new ChannelManager();
+    this.allPeers = new Map();
+    this.userSessionMgr = new UserSessionManager({
+      UserInfoClass: UserInfo,
+      onSessionLoggedIn: (newSession => {
+        // console.log('=================== newSession :', newSession);
+        newSession.data.rcPeer.setSession(newSession);
+      }),
+      onUserLoggedIn: (newUser => {
+        newUser.data.userSessionManager = this;
+        // console.log('=================== newUser :', newUser);
+      }),
+      onSessionLoggedOut: ((existedSession, reason) => {
+      }),
+      onUserLoggedOut: (existedUser => {
+      }),
+      onSessionUnexpectedLoggedOut: ((existedSession, reason) => {
+      }),
+      onSessionReloggedIn: ((reloggedInSession, newData) => {
+      }),
+      onSessionDuplicateLogin: ((existedSession, newSession, logoutExistedOne, denyLogin) => {
+        denyLogin();
+        logoutExistedOne();
+      }),
+    });
+  }
+
+  addPeer(rcPeer) {
+    let wsPeer = rcPeer.getWsPeer();
+    if(wsPeer){
+      this.allPeers.set(rcPeer.getWsPeer(), rcPeer);
+    }
+  }
+
+  loginWithToken(rcPeer, token) {
+    // const session = fakeUserManager.authenticateFromToken(data.token);
+    const session = fakeUserManager.verify(token);
+    if (session) {
+      console.log('session.user_id :', session.user_id);
+      const wsSessionNumber = this.userSessionCounters[session.user_id] = this.userSessionCounters[session.user_id] ?
+        this.userSessionCounters[session.user_id] + 1 : 1;
+
+      const wsSession = fakeUserManager.authenticateFromToken(token, { wsSessionNumber });
+
+      return this.userSessionMgr.login(wsSession.user_id, wsSession.token, {
+        session,
+        rcPeer,
+      })
+      .then(() => wsSession);
+    }
+    return Promise.reject();
+  }
+
+  loginWithPassword(rcPeer, body){
+    const { auth_type, password, username } = body;
+    return Promise.resolve(fakeUserManager.authenticate(auth_type, username, password));
+  }
+
+  get users(){
+    return this.userSessionMgr.users;
+  }
+
+  mapUser(inFn){
+    let users = this.userSessionMgr.users;
+    let fn = inFn || (() => {});
+    return Object.keys(users).map(key => {
+      return fn(key, users[key], users);
+    });
+  }
+
+  findUser(uid){
+    return this.users[uid];
+  }
+
+  peerJoinChannel(user, channelArray){
+    let result = this.chManager.join(user, channelArray);
+    user.data.channelMap = this.chManager.getUserMetadata(user);
+    return result;
+  }
+
+  peerLeaveChannel(user, channelArray){
+    let result = this.chManager.leave(user, channelArray);
+    user.data.channelMap = this.chManager.getUserMetadata(user);
+    return result;
+  }
+
+  peerLeaveAllChannel(user){
+    let result = this.chManager.leaveAll(user);
+    user.data.channelMap = this.chManager.getUserMetadata(user);
+    return result;
+  }
+
+  isPeerInChannel(user, channel){
+    return this.chManager.isInChannel(user, channel)
+  }
+
+  getPeerChannelList(user){
+    return this.chManager.getUserMetadata(user).map(value => value);
+  }
+
+  removePeer(rcPeer) {
+    let wsPeer = rcPeer.getWsPeer();
+    if(wsPeer){
+      this.allPeers.delete(rcPeer.getWsPeer());
+    }
+  }
+
+  logout(rcPeer) {
+    this.removePeer(rcPeer);
+    let sessionId = rcPeer.getSessionId();
+    if(sessionId){
+      // console.log('removePeer');
+      return this.userSessionMgr.logout(sessionId);
+    }
+    return Promise.resolve();
+  }
+
+  unexpectedLogout(rcPeer) {
+    console.log('unexpectedLogout');
+    this.removePeer(rcPeer);
+    let sessionId = rcPeer.getSessionId();
+    if(sessionId){
+      return this.userSessionMgr.unexpectedLogout(sessionId, 'ConnectionLost');
+    }
+    return Promise.resolve();
+  }
+}

src/server/utils/fakeUserManager.js
@@ -19,7 +19,7 @@ class FakeUserManager {
         user_name: user.name,
         auth_type,
         privilege: user.privilege,
-        subject: `user:${user.id}:${0}`,
+        subject: `user:${user.id}:${rest.wsSessionNumber || 0}`,
         token_type: 'Bearer',
         ...rest,
       }),
@@ -33,6 +33,10 @@ class FakeUserManager {
     });
 
     this.register('admin', 'admin', 'Admin', 'admin');
+    
+    for (let index = 1; index <= 10; index++) {
+      this.register(`testUser${index}`, `testUser${index}`, `Test User ${index}`, 'user');
+    }
   }
 
   register(username, password, name, privilege){
@@ -53,15 +57,36 @@ class FakeUserManager {
     return user;
   }
 
-  authenticate(auth_type, username, password){
+  authenticate(auth_type, username, password, extraPayload){
     const user = this.usernames[username];
     if(auth_type !== 'basic' || !user){
       return null;
     }
 
     let session = this.jwtSessionHelper.createSession({
+      ...extraPayload,
       user,
       auth_type,
+    });
+
+    return session.info;
+  }
+
+  authenticateFromToken(token, extraPayload){
+    const tokenInfo = this.verify(token);
+    if(!tokenInfo){
+      return null;
+    }
+
+    const user = this.userIdMap[tokenInfo.user_id];
+    if(!user){
+      return null;
+    }
+
+    let session = this.jwtSessionHelper.createSession({
+      ...extraPayload,
+      user,
+      auth_type: tokenInfo.auth_type,
     });
 
     return session.info;